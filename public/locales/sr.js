584e0d04f4b54ecbaf4a0bc367428511fd0d0448
Anarhista <41642197+Anarhista@users.noreply.github.com>
2018-10-27 01:33:25 +0200

    Update sr.js

--- public/locales/sr.js
+++ public/locales/sr.js
@@ -3,7 +3,7 @@ var allLocales = {
     copyAgain :             "Molimo vas da kopirate seed iz backupa.",
     bothFields :            "Oba polja su obavezna.",
     allFields :             "Sva polja su obavezna.",
-    wrongSeed :             "Niste upisali dobre seed riječi.",
+    wrongSeed :             "Niste upisali dobre seed reči.",
     passNotMatch :          "Lozinke koje ste upisali nisu iste.",
     nicknameExists :        "Ovaj nadimak je već u upotrebi.",
     emailExists :           "Ova email adresa je već u upotrebi."
